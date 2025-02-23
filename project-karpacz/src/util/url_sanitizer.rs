28cc287fd9c0d4c8c0aeec87392e780f7aefcf41
Dorian Niemiec <dorian.niemiec@svrjs.org>
2025-02-23 21:23:58 +0100

    docs: add SVR.JS copyright notice to the path sanitizer

--- project-karpacz/src/util/url_sanitizer.rs
+++ project-karpacz/src/util/url_sanitizer.rs
@@ -1,3 +1,16 @@
+// Copyright (c) 2018-2025 SVR.JS
+// Portions of this file are derived from SVR.JS (https://git.svrjs.org/svrjs/svrjs).
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
 use anyhow::{anyhow, Result};
 use std::str;
 
