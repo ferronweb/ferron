8156de2dbaf75b5a86dc61d8837bd60933a6a2b5
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-06-22 13:18:02 +0200

    fix: fix host configurations not being used

--- ferron/src/config/mod.rs
+++ ferron/src/config/mod.rs
@@ -160,9 +160,9 @@ impl ServerConfigurationFilters {
 
 impl Ord for ServerConfigurationFilters {
   fn cmp(&self, other: &Self) -> Ordering {
-    if self.port.is_none() && other.port.is_some() {
+    if self.hostname.is_none() && other.hostname.is_some() {
       Ordering::Less
-    } else if self.port.is_some() && other.port.is_none() {
+    } else if self.hostname.is_some() && other.hostname.is_none() {
       Ordering::Greater
     } else if self.ip.is_none() && other.ip.is_some() {
       Ordering::Less
