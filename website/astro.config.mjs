48752853a9e337cb19eb7157ed65a7ab5a7b7792
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-07-06 08:32:21 +0300

    chore: disable HTML compression in `astro-compress`

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -23,7 +23,9 @@ export default defineConfig({
     react(),
     sitemap(),
     pagefind(),
-    (await import("astro-compress")).default()
+    (await import("astro-compress")).default({
+      HTML: false
+    })
   ],
   markdown: {
     shikiConfig: {
