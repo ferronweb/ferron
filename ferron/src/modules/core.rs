4565d0793a7318fcc2d711157894472cab1ac58a
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-07-26 09:41:27 +0200

    fix: improve HTTPS configuration detection even more to fix erroneous HTTP to HTTPS redirects

--- ferron/src/modules/core.rs
+++ ferron/src/modules/core.rs
@@ -787,7 +787,8 @@ impl ModuleHandlers for CoreModuleHandlers {
         && config.filters.port.is_none() // Port is set implicitly
         && (get_value!("auto_tls", config) // TLS is enabled
           .and_then(|v| v.as_bool())
-          .unwrap_or(!is_localhost(config.filters.ip.as_ref(), config.filters.hostname.as_deref()))
+          .unwrap_or((config.filters.hostname.is_some() || config.filters.ip.is_some()) &&
+              !is_localhost(config.filters.ip.as_ref(), config.filters.hostname.as_deref()))
           || config.entries.contains_key("tls"))
       {
         if let Some(default_http_port) = self.default_http_port {
