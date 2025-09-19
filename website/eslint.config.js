19f49cab3cee41ae4f4158067184c946d3afb954
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-09-19 17:41:58 +0200

    chore(website): add `typescript-eslint` to linting setup, and enforce TypeScript

--- website/eslint.config.js
+++ website/eslint.config.js
@@ -1,10 +1,28 @@
+import { defineConfig } from "eslint/config";
 import eslintPluginAstro from "eslint-plugin-astro";
 import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import globals from "globals";
 
-export default [
+export default defineConfig(
   // add more generic rule sets here, such as:
   js.configs.recommended,
+  tseslint.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module"
+      }
+    },
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    }
+  },
   {
     rules: {
       // override/add rules settings here, such as:
@@ -12,4 +30,4 @@ export default [
     }
   },
   { ignores: ["dist", ".astro"] }
-];
+);
--- website/lint-staged.config.js
+++ website/lint-staged.config.js
@@ -3,5 +3,6 @@ export default {
   "../blog/**/*.md": ["eslint --cache --fix", "prettier --write"],
   "src/**/*.md": ["eslint --cache --fix", "prettier --write"],
   "src/**/*.js": ["eslint --cache --fix", "prettier --write"],
+  "src/**/*.ts": ["eslint --cache --fix", "prettier --write"],
   "src/**/*.astro": ["eslint --cache --fix", "prettier --write"]
 };
