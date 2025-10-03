188a2def677bf89ccac8a2bca0049ea7563fbbf6
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-10-03 19:26:21 +0200

    test: don't allocate a pseudo-TTY when generating TLS key pairs

--- dockertest/tests/rproxy-tls/run.sh
+++ dockertest/tests/rproxy-tls/run.sh
@@ -2,7 +2,7 @@
 TEST_FAILED=0
 rm -rf certs
 mkdir -p certs
-docker run -t -i --rm -v ./certs:/etc/certs alpine/openssl req -x509 -newkey rsa:4096 -keyout /etc/certs/server.key -out /etc/certs/server.crt -days 365 -nodes -subj "/CN=localhost" > /dev/null || (echo "Failed to generate TLS key pair for a test suite" >&2; exit 1)
+docker run --rm -v ./certs:/etc/certs alpine/openssl req -x509 -newkey rsa:4096 -keyout /etc/certs/server.key -out /etc/certs/server.crt -days 365 -nodes -subj "/CN=localhost" > /dev/null 2>&1 || (echo "Failed to generate TLS key pair for a test suite" >&2; exit 1)
 docker compose --progress quiet up --build -d > /dev/null || (echo "Failed to start containers for a test suite" >&2; exit 1)
 cat test.sh | docker compose --progress quiet exec -T test-runner bash 2>&1 || TEST_FAILED=1
 docker compose --progress quiet kill -s SIGKILL > /dev/null || true
