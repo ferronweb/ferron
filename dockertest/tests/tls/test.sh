de66b1ef9c73591483938b7dc71f3575ce884dac
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-10-04 10:29:25 +0200

    test: test TLS connections with HTTP/1.1, HTTP/2 and HTTP/3

--- dockertest/tests/tls/test.sh
+++ dockertest/tests/tls/test.sh
@@ -2,12 +2,32 @@
 
 TEST_FAILED=0
 
-TEST_RESULTS="$(curl -fsSLk -o /dev/null https://ferron/)"
+TEST_RESULTS="$(curl -fsSLk --http1.1 -o /dev/null https://ferron/)"
 TEST_EXIT_CODE=$?
 if [ "$TEST_EXIT_CODE" -eq 0 ]; then
-    echo "TLS connection test passed!"
+    echo "HTTP/1.1 via TLS connection test passed!"
 else
-    echo "TLS connection test failed!" >&2
+    echo "HTTP/1.1 via TLS connection test failed!" >&2
+    echo "  Exit code: $TEST_EXIT_CODE" >&2
+    TEST_FAILED=1
+fi
+
+TEST_RESULTS="$(curl -fsSLk --http2-prior-knowledge -o /dev/null https://ferron/)"
+TEST_EXIT_CODE=$?
+if [ "$TEST_EXIT_CODE" -eq 0 ]; then
+    echo "HTTP/2 via TLS connection test passed!"
+else
+    echo "HTTP/2 via TLS connection test failed!" >&2
+    echo "  Exit code: $TEST_EXIT_CODE" >&2
+    TEST_FAILED=1
+fi
+
+TEST_RESULTS="$(curl -fsSLk --http3-only -o /dev/null https://ferron/)"
+TEST_EXIT_CODE=$?
+if [ "$TEST_EXIT_CODE" -eq 0 ]; then
+    echo "HTTP/3 via TLS connection test passed!"
+else
+    echo "HTTP/3 via TLS connection test failed!" >&2
     echo "  Exit code: $TEST_EXIT_CODE" >&2
     TEST_FAILED=1
 fi
