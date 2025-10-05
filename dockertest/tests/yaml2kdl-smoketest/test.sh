3f612969e07803d68eb5182bb8cbda19ded16938
Dorian Niemiec <dorian.niemiec@ferronweb.org>
2025-10-05 14:08:10 +0200

    test: add YAML to KDL conversion smoke test

---
+++ dockertest/tests/yaml2kdl-smoketest/test.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+TEST_FAILED=0
+
+# Wait for the HTTP server to start
+for i in $(seq 1 3)
+do
+    if [ "$i" -gt 1 ]; then
+        sleep 1
+    fi
+    nc -z ferron 80 >/dev/null 2>&1 && break || true
+done
+
+TEST_RESULTS="$(curl -fsSL http://ferron/basic.txt)"
+TEST_EXIT_CODE=$?
+TEST_EXPECTED="$(cat /var/www/ferron/basic.txt)"
+if [ "$TEST_EXIT_CODE" -eq 0 ] && [ "$TEST_RESULTS" = "$TEST_EXPECTED" ]; then
+    echo "YAML to KDL conversion smoke test passed!"
+else
+    echo "YAML to KDL conversion smoke test failed!" >&2
+    echo "  Exit code: $TEST_EXIT_CODE" >&2
+    echo "  Expected: $TEST_EXPECTED" >&2
+    echo "  Received: $TEST_RESULTS" >&2
+    TEST_FAILED=1
+fi
+
+if [ "$TEST_FAILED" -eq 1 ]; then
+    exit 1
+fi
