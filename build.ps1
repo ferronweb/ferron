5af15ebbc5d08ad2e277e16ebf32a06d149da75f
Dorian Niemiec <dorian@ferron.sh>
2025-10-10 12:27:20 +0200

    build: format and fix the PowerShell scripts [skip ci]

--- build.ps1
+++ build.ps1
@@ -1,6 +1,7 @@
 # Get version from Cargo.toml
 $ferronVersionCargo = Select-String -Path "ferron/Cargo.toml" -Pattern '^version' | ForEach-Object {
-    if ($_ -match '"([0-9a-zA-Z.+-]+)"') {
+    if ($_ -match '"([0-9a-zA-Z.+-]+)"')
+    {
         $matches[1]
     }
 }
@@ -11,18 +12,24 @@ $ferronVersionGit = git tag --sort=-committerdate | Select-Object -First 1 | For
 }
 
 # Use Cargo version if available, otherwise fallback to git version
-$ferronVersion = if ($ferronVersionCargo) { $ferronVersionCargo } else { $ferronVersionGit }
+$ferronVersion = if ($ferronVersionCargo)
+{ $ferronVersionCargo
+} else
+{ $ferronVersionGit
+}
 
 # Get host target triple
 $hostTargetTriple = rustc -vV | Where-Object { $_ -match '^host: ' } | ForEach-Object { $_ -replace 'host: ', '' }
 
 # Check if $env:TARGET is set
-if ($env:TARGET) {
+if ($env:TARGET)
+{
     $cargoFinalExtraArgs = "--target $env:TARGET"
     $cargoTargetRoot = "target/$env:TARGET"
     $destTargetTriple = $env:TARGET
     $buildRelease = "build-release-$env:TARGET"
-} else {
+} else
+{
     $cargoFinalExtraArgs = ""
     $cargoTargetRoot = "target"
     $destTargetTriple = $hostTargetTriple
@@ -30,7 +37,8 @@ if ($env:TARGET) {
 }
 
 # Append extra arguments if set
-if ($env:CARGO_FINAL_EXTRA_ARGS) {
+if ($env:CARGO_FINAL_EXTRA_ARGS)
+{
     $cargoFinalExtraArgs = "$cargoFinalExtraArgs $env:CARGO_FINAL_EXTRA_ARGS"
 }
 
@@ -41,35 +49,42 @@ $cargoFinalExtraArgs = "--no-default-features -F ferron/runtime-tokio $cargoFina
 $cargoFinalExtraArgs = $cargoFinalExtraArgs -Split ' '
 
 # Set cargo executable if not set
-if (-not $env:CARGO_FINAL) {
+if (-not $env:CARGO_FINAL)
+{
     $cargoFinal = "cargo"
-} else {
+} else
+{
     $cargoFinal = $env:CARGO_FINAL
 }
 
-function Run {
+function Run
+{
     Build
     & "$cargoTargetRoot/release/ferron"
 }
 
-function RunDev {
+function RunDev
+{
     BuildDev
     & "$cargoTargetRoot/debug/ferron"
 }
 
-function Smoketest {
+function Smoketest
+{
     Build
     $env:FERRON = $PWD.Path + '\' + $cargoTargetRoot + '\release\ferron'
     & powershell.exe -ExecutionPolicy Bypass ".\smoketest\smoketest.ps1"
 }
 
-function SmoketestDev {
+function SmoketestDev
+{
     BuildDev
     $env:FERRON = $PWD.Path + '\' + $cargoTargetRoot + '\debug\ferron'
     & powershell.exe -ExecutionPolicy Bypass ".\smoketest\smoketest.ps1"
 }
 
-function Build {
+function Build
+{
     PrepareBuild
     FixConflicts
     Push-Location build-workspace
@@ -77,7 +92,8 @@ function Build {
     Pop-Location
 }
 
-function BuildDev {
+function BuildDev
+{
     PrepareBuild
     FixConflicts
     Push-Location build-workspace
@@ -85,24 +101,34 @@ function BuildDev {
     Pop-Location
 }
 
-function PrepareBuild {
+function PrepareBuild
+{
     & cargo run --manifest-path build-prepare/Cargo.toml
 }
 
-function FixConflicts {
+function FixConflicts
+{
     Push-Location build-workspace
-    while (($oldConflictingPackages -ne $conflictingPackages) -or (-not $oldConflictingPackages)) {
+    while (($oldConflictingPackages -ne $conflictingPackages) -or (-not $oldConflictingPackages))
+    {
         $oldConflictingPackages = $conflictingPackages
         $conflictingPackages = (cargo update -w --dry-run 2>&1) | Select-String -Pattern '^error: failed to select a version for (?:the requirement )?`([^ `]+)' | ForEach-Object { $_.Matches.Groups[1].Value }
         $conflictingPackages = $conflictingPackages -Split ' '
-        if (-not $conflictingPackages) { break }
-        if ($oldConflictingPackages -eq $conflictingPackages) { throw "Couldn't resolve Cargo conflicts" }
-        if ($conflictingPackages) { & cargo update $conflictingPackages }
+        if (-not $conflictingPackages)
+        { break
+        }
+        if ($oldConflictingPackages -eq $conflictingPackages)
+        { throw "Couldn't resolve Cargo conflicts"
+        }
+        if ($conflictingPackages)
+        { & cargo update $conflictingPackages
+        }
     }
     Pop-Location
 }
 
-function Package {
+function Package
+{
     Remove-Item -Recurse -Force -ErrorAction SilentlyContinue $buildRelease
     New-Item -ItemType Directory -Path $buildRelease | Out-Null
 
@@ -115,29 +141,37 @@ function Package {
     Copy-Item ferron-release.kdl "$buildRelease/ferron.kdl" -Force
     Copy-Item wwwroot -Destination $buildRelease -Recurse -Force
 
-    if (-not (Test-Path "dist")) { New-Item -ItemType Directory -Path "dist" | Out-Null }
-    if (Test-Path "dist/ferron-$ferronVersion-$destTargetTriple.zip") { Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "dist/ferron-$ferronVersion-$destTargetTriple.zip" }
+    if (-not (Test-Path "dist"))
+    { New-Item -ItemType Directory -Path "dist" | Out-Null
+    }
+    if (Test-Path "dist/ferron-$ferronVersion-$destTargetTriple.zip")
+    { Remove-Item -Recurse -Force -ErrorAction SilentlyContinue "dist/ferron-$ferronVersion-$destTargetTriple.zip"
+    }
 
     Compress-Archive -Path "$buildRelease\*" -DestinationPath "dist/ferron-$ferronVersion-$destTargetTriple.zip"
 
     Remove-Item -Recurse -Force $buildRelease
 }
 
-function BuildWithPackage {
+function BuildWithPackage
+{
     Build
     Package
 }
 
-function Clean {
-    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue build-workspace, build-release, dist, packaging/deb/ferron_* packaging/deb/md5sums.tmp,
+function Clean
+{
+    Remove-Item -Recurse -Force -ErrorAction SilentlyContinue build-workspace, build-release, dist, packaging/deb/ferron_* packaging/deb/md5sums.tmp
     & cargo clean
     Push-Location build-prepare
     & cargo clean
     Pop-Location
 }
 
-if ($args.Count -gt 0) {
+if ($args.Count -gt 0)
+{
     & $args[0]
-} else {
+} else
+{
     Write-Host "Available commands: Run, RunDev, Build, BuildDev, Smoketest, SmoketestDev, PrepareBuild, FixConflicts, Package, BuildWithPackage, Clean"
 }
