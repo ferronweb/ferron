33b10b12920d51cd08d5a1d1a370fa4978f67830
Dorian Niemiec <dorian@ferron.sh>
2025-10-14 14:13:35 +0200

    test: add tests for least connections load balancing algorithm

--- dockertest/tests/lb/test.sh
+++ dockertest/tests/lb/test.sh
@@ -101,6 +101,20 @@ else
     TEST_FAILED=1
 fi
 
+TEST_RESULTS="$(curl -fsSL http://ferron-least-conn/ && echo -n " " && curl -fsSL http://ferron-least-conn/ && echo -n " " && curl -fsSL http://ferron-least-conn/)"
+TEST_EXIT_CODE=$?
+TEST_EXPECTED="Hello, World! Hello, World! Hello, World!"
+if [ "$TEST_EXIT_CODE" -eq 0 ] && [ "$TEST_RESULTS" = "$TEST_EXPECTED" ]; then
+    echo "Basic load balancing (least connections) test passed!"
+else
+    echo "Basic load balancing (least connections) test failed!" >&2
+    echo "  Exit code: $TEST_EXIT_CODE" >&2
+    echo "  Expected: $TEST_EXPECTED" >&2
+    echo "  Received: $TEST_RESULTS" >&2
+    TEST_FAILED=1
+fi
+
+
 if [ "$TEST_FAILED" -eq 1 ]; then
     exit 1
 fi
