e7bd1b16e27bfb4aad02c4f82c3e2d52b6a35c94
Alex <al-git001@none.at>
2025-10-30 14:10:20 +0100

    Add lookup_env_var to get environment Variable, after feedback
    Signed-off-by: Alex <al-git001@none.at>

--- ferron-common/src/util/lookup_env_var.rs
+++ ferron-common/src/util/lookup_env_var.rs
@@ -1,25 +1,31 @@
 use std::env;
+use anyhow::bail;
 
-pub fn lookup_env_value(conf_string: String) -> String {
-    let index_lb = conf_string.find("{");
-    if let Some(index_lb) = index_lb {
-        let index_rb_afterlb = conf_string.find("}");
-        if let Some(index_rb_afterlb) = index_rb_afterlb {
-            // +1 for {
-            // +4 for env:
-            return get_env_val(conf_string[index_lb + 5..index_rb_afterlb].to_string());
-        } else {
-            return conf_string;
-        }
-    } else {
-        return conf_string;
+pub fn lookup_env_value(conf_string: String) -> anyhow::Result<String> {
+    if !conf_string.starts_with("{env:") {
+        bail!("String '{}' does not start with '{{env:'", conf_string);
     }
+
+    // Search Index of "{env:"
+    if let Some(index_lb) = conf_string.find("{env:") {
+        // Search for ending "}"
+        if let Some(index_rb_afterlb) = conf_string[index_lb + 5..].find("}") {
+            return get_env_val(
+                conf_string[index_lb + 5..index_lb + 5 + index_rb_afterlb].to_string(),
+            );
+        } else {
+            bail!("No closing '}}' found in string '{}'", conf_string);
+        }
+    }
+
+    // If not an env prefix return the string
+    Ok(conf_string)
 }
 
-pub fn get_env_val(conf_key: String) -> String {
+pub fn get_env_val(conf_key: String) -> anyhow::Result<String> {
     match env::var(conf_key.clone()) {
-        Ok(val) => return val,
-        Err(e) => format!("couldn't find ENV-Key >>{conf_key}<< {e}"),
+        Ok(val) => return Ok(val),
+        Err(e) => bail!("couldn't find ENV-Key >>{conf_key}<< {e}"),
     }
 }
 
@@ -30,29 +36,35 @@ mod tests {
     #[test]
     fn lala_get_env_var() {
         let result = get_env_val("LALA".to_string());
-        assert_eq!(result, "couldn't find ENV-Key >>LALA<< environment variable not found");
+        assert!(
+            result.is_err(),
+            "couldn't find ENV-Key >>LALA<< environment variable not found"
+        );
     }
 
     #[test]
     fn home_get_env_var() {
         let result = get_env_val("HOME".to_string());
-        assert_ne!(result, "");
+        assert!(result.is_ok(), "Found 'HOME'");
     }
 
     #[test]
     fn missing_get_env_key() {
         let result = lookup_env_value("LALA".to_string());
-        assert_eq!(result, "LALA");
+        assert!(result.is_err(), "Not found LALA");
     }
 
     #[test]
     fn success_lookup_env_value() {
         let result = lookup_env_value("{env:HOME}".to_string());
-        assert_ne!(result, "");
+        assert!(result.is_ok(), "Get successfully 'HOME' ENV");
     }
     #[test]
     fn failed_lookup_env_value() {
         let result = lookup_env_value("{envA:HOME}".to_string());
-        assert_eq!(result, "couldn't find ENV-Key >>:HOME<< environment variable not found");
+        assert!(
+            result.is_err(),
+            "couldn't find ENV-Key >>:HOME<< environment variable not found"
+        );
     }
 }
