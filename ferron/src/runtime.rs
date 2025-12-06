d54433d422b9eaa576ee431673e08f7d0a7dbb4e
Dorian Niemiec <dorian@ferron.sh>
2025-12-06 10:01:41 +0100

    revert: revert use of IOCP Monoio

--- ferron/src/runtime.rs
+++ ferron/src/runtime.rs
@@ -10,15 +10,6 @@ compile_error!("Can't compile Ferron with no main runtimes enabled");
 /// Creates a new asynchronous runtime using Monoio
 #[cfg(feature = "runtime-monoio")]
 pub fn new_runtime(future: impl Future, enable_uring: bool) -> Result<(), Box<dyn Error + Send + Sync>> {
-  #[cfg(windows)]
-  {
-    let mut rt = monoio::RuntimeBuilder::<monoio::IocpDriver>::new()
-      .enable_all()
-      .attach_thread_pool(Box::new(BlockingThreadPool))
-      .build()?;
-    rt.block_on(future);
-  }
-  #[cfg(not(windows))]
   if enable_uring && monoio::utils::detect_uring() {
     #[cfg(target_os = "linux")]
     let mut rt = monoio::RuntimeBuilder::<monoio::IoUringDriver>::new()
