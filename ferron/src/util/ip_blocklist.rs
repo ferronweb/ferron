6c6b9e5db6d7c08fe5fc62776b316c084e2ea4d6
Dorian Niemiec <dorian@ferron.sh>
2025-12-21 23:47:36 +0100

    feat: accept CIDR ranges for IP blocklists directives (backported from Ferron 2)

--- ferron/src/util/ip_blocklist.rs
+++ ferron/src/util/ip_blocklist.rs
@@ -1,37 +1,72 @@
+use std::cmp::Ordering;
 use std::collections::HashSet;
 use std::net::{IpAddr, Ipv6Addr};
 
+use cidr::IpCidr;
+
+/// The IP blocklist
+#[derive(Clone, Debug, PartialEq, Eq)]
 pub struct IpBlockList {
   blocked_ips: HashSet<IpAddr>,
+  blocked_cidrs: HashSet<IpCidr>,
+}
+
+impl Default for IpBlockList {
+  fn default() -> Self {
+    Self::new()
+  }
+}
+
+impl Ord for IpBlockList {
+  fn cmp(&self, other: &Self) -> Ordering {
+    self
+      .blocked_ips
+      .iter()
+      .cmp(other.blocked_ips.iter())
+      .then(self.blocked_cidrs.iter().cmp(other.blocked_cidrs.iter()))
+  }
+}
+
+impl PartialOrd for IpBlockList {
+  fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+    Some(self.cmp(other))
+  }
 }
 
 impl IpBlockList {
-  // Create a new empty block list
+  /// Creates a new empty block list
   pub fn new() -> Self {
     Self {
       blocked_ips: HashSet::new(),
+      blocked_cidrs: HashSet::new(),
     }
   }
 
-  // Load the block list from a vector of IP address strings
+  /// Loads the block list from a vector of IP address strings
   pub fn load_from_vec(&mut self, ip_list: Vec<&str>) {
     for ip_str in ip_list {
       match ip_str {
         "localhost" => {
-          self.blocked_ips.insert(Ipv6Addr::LOCALHOST.into());
+          self.blocked_ips.insert(IpAddr::V6(Ipv6Addr::LOCALHOST));
         }
         _ => {
           if let Ok(ip) = ip_str.parse::<IpAddr>() {
             self.blocked_ips.insert(ip.to_canonical());
+          } else if let Ok(ip_cidr) = ip_str.parse::<IpCidr>() {
+            self.blocked_cidrs.insert(ip_cidr);
           }
         }
       }
     }
   }
 
-  // Check if an IP address is blocked
+  /// Checks if an IP address is blocked
   pub fn is_blocked(&self, ip: IpAddr) -> bool {
     self.blocked_ips.contains(&ip.to_canonical())
+      || self
+        .blocked_cidrs
+        .iter()
+        .any(|cidr| cidr.contains(&ip.to_canonical()))
   }
 }
 
@@ -43,6 +78,16 @@ mod tests {
   fn test_ip_block_list() {
     let mut block_list = IpBlockList::new();
     block_list.load_from_vec(vec!["192.168.1.1", "10.0.0.1"]);
+
+    assert!(block_list.is_blocked("192.168.1.1".parse().unwrap()));
+    assert!(block_list.is_blocked("10.0.0.1".parse().unwrap()));
+    assert!(!block_list.is_blocked("8.8.8.8".parse().unwrap()));
+  }
+
+  #[test]
+  fn test_ip_cidr_block_list() {
+    let mut block_list = IpBlockList::new();
+    block_list.load_from_vec(vec!["192.168.1.0/24", "10.0.0.0/8"]);
 
     assert!(block_list.is_blocked("192.168.1.1".parse().unwrap()));
     assert!(block_list.is_blocked("10.0.0.1".parse().unwrap()));
