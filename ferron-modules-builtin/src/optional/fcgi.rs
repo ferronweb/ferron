1086008daa36d78ca4da8e38f938972fad10fa1c
Dorian Niemiec <dorian@ferron.sh>
2026-01-01 15:38:13 +0100

    fix: correct the `HTTPS` environment variable value for CGI, SCGI and FastCGI
    This addresses the issue #371

--- ferron-modules-builtin/src/optional/fcgi.rs
+++ ferron-modules-builtin/src/optional/fcgi.rs
@@ -743,7 +743,7 @@ async fn execute_fastcgi_with_environment_variables(
   }
 
   if socket_data.encrypted {
-    environment_variables.insert("HTTPS".to_string(), "ON".to_string());
+    environment_variables.insert("HTTPS".to_string(), "on".to_string());
   }
 
   for (header_name, header_value) in request.headers().iter() {
--- ferron-modules-builtin/src/optional/scgi.rs
+++ ferron-modules-builtin/src/optional/scgi.rs
@@ -389,7 +389,7 @@ async fn execute_scgi_with_environment_variables(
   }
 
   if socket_data.encrypted {
-    environment_variables.insert("HTTPS".to_string(), "ON".to_string());
+    environment_variables.insert("HTTPS".to_string(), "on".to_string());
   }
 
   let mut content_length_set = false;
