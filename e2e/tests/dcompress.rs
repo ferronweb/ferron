8803b677db199d7af29bd718151800d0224aa574
Dorian Niemiec <dorian@ferron.sh>
2026-03-24 09:17:59 +0100

    test: add more compression algorithms for dynamic compression test

--- e2e/tests/dcompress.rs
+++ e2e/tests/dcompress.rs
@@ -95,4 +95,191 @@ async fn test_dynamic_compression_gzip() {
   container.stop().await.unwrap();
 }
 
-// TODO: deflate, brotli, zstd
+#[tokio::test]
+async fn test_dynamic_compression_deflate() {
+  #[cfg(unix)]
+  nix::sys::stat::umask(nix::sys::stat::Mode::from_bits(0o000).unwrap());
+
+  #[cfg(unix)]
+  let webroot_dir = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o777))
+    .tempdir()
+    .unwrap();
+  #[cfg(unix)]
+  let mut config_file = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o666))
+    .tempfile()
+    .unwrap();
+  #[cfg(not(unix))]
+  let webroot_dir = tempfile::tempdir().unwrap();
+  #[cfg(not(unix))]
+  let mut config_file = tempfile::NamedTempFile::new().unwrap();
+
+  config_file
+    .as_file_mut()
+    .write_all(
+      r#"
+:80 {
+  root "/var/www/ferron"
+
+  compressed #false
+  dynamic_compressed
+}
+"#
+      .as_bytes(),
+    )
+    .unwrap();
+
+  self::common::write_file(webroot_dir.path().join("small.txt"), b"test content").unwrap();
+
+  let container = create_ferron_container(webroot_dir.path(), config_file.path())
+    .await
+    .unwrap();
+
+  let port = container.get_host_port_ipv4(ContainerPort::Tcp(80)).await.unwrap();
+  let client = reqwest::Client::new();
+
+  // Test gzip compression
+  let response = client
+    .get(format!("http://localhost:{}/small.txt", port))
+    .header("Accept-Encoding", "deflate")
+    .send()
+    .await
+    .unwrap();
+
+  assert_eq!(response.status(), reqwest::StatusCode::OK);
+  let bytes = response.bytes().await.unwrap();
+  let mut decompressed = Vec::new();
+  let _ = flate2::read::DeflateDecoder::new(bytes.as_ref())
+    .read_to_end(&mut decompressed)
+    .unwrap();
+  assert_eq!(decompressed, b"test content");
+
+  container.stop().await.unwrap();
+}
+
+#[tokio::test]
+async fn test_dynamic_compression_brotli() {
+  #[cfg(unix)]
+  nix::sys::stat::umask(nix::sys::stat::Mode::from_bits(0o000).unwrap());
+
+  #[cfg(unix)]
+  let webroot_dir = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o777))
+    .tempdir()
+    .unwrap();
+  #[cfg(unix)]
+  let mut config_file = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o666))
+    .tempfile()
+    .unwrap();
+  #[cfg(not(unix))]
+  let webroot_dir = tempfile::tempdir().unwrap();
+  #[cfg(not(unix))]
+  let mut config_file = tempfile::NamedTempFile::new().unwrap();
+
+  config_file
+    .as_file_mut()
+    .write_all(
+      r#"
+:80 {
+  root "/var/www/ferron"
+
+  compressed #false
+  dynamic_compressed
+}
+"#
+      .as_bytes(),
+    )
+    .unwrap();
+
+  self::common::write_file(webroot_dir.path().join("small.txt"), b"test content").unwrap();
+
+  let container = create_ferron_container(webroot_dir.path(), config_file.path())
+    .await
+    .unwrap();
+
+  let port = container.get_host_port_ipv4(ContainerPort::Tcp(80)).await.unwrap();
+  let client = reqwest::Client::new();
+
+  // Test gzip compression
+  let response = client
+    .get(format!("http://localhost:{}/small.txt", port))
+    .header("Accept-Encoding", "br")
+    .send()
+    .await
+    .unwrap();
+
+  assert_eq!(response.status(), reqwest::StatusCode::OK);
+  let mut bytes = std::io::Cursor::new(response.bytes().await.unwrap());
+  let mut decompressed = Vec::new();
+
+  brotli::BrotliDecompress(&mut bytes, &mut decompressed).unwrap();
+  assert_eq!(decompressed, b"test content");
+
+  container.stop().await.unwrap();
+}
+
+#[tokio::test]
+async fn test_dynamic_compression_zstd() {
+  #[cfg(unix)]
+  nix::sys::stat::umask(nix::sys::stat::Mode::from_bits(0o000).unwrap());
+
+  #[cfg(unix)]
+  let webroot_dir = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o777))
+    .tempdir()
+    .unwrap();
+  #[cfg(unix)]
+  let mut config_file = tempfile::Builder::new()
+    .permissions(Permissions::from_mode(0o666))
+    .tempfile()
+    .unwrap();
+  #[cfg(not(unix))]
+  let webroot_dir = tempfile::tempdir().unwrap();
+  #[cfg(not(unix))]
+  let mut config_file = tempfile::NamedTempFile::new().unwrap();
+
+  config_file
+    .as_file_mut()
+    .write_all(
+      r#"
+:80 {
+  root "/var/www/ferron"
+
+  compressed #false
+  dynamic_compressed
+}
+"#
+      .as_bytes(),
+    )
+    .unwrap();
+
+  self::common::write_file(webroot_dir.path().join("small.txt"), b"test content").unwrap();
+
+  let container = create_ferron_container(webroot_dir.path(), config_file.path())
+    .await
+    .unwrap();
+
+  let port = container.get_host_port_ipv4(ContainerPort::Tcp(80)).await.unwrap();
+  let client = reqwest::Client::new();
+
+  // Test gzip compression
+  let response = client
+    .get(format!("http://localhost:{}/small.txt", port))
+    .header("Accept-Encoding", "zstd")
+    .send()
+    .await
+    .unwrap();
+
+  assert_eq!(response.status(), reqwest::StatusCode::OK);
+  let mut bytes = std::io::Cursor::new(response.bytes().await.unwrap());
+  let mut decompressed = Vec::new();
+
+  let mut decoder = zstd::Decoder::new(&mut bytes).unwrap();
+  std::io::copy(&mut decoder, &mut decompressed).unwrap();
+
+  assert_eq!(decompressed, b"test content");
+
+  container.stop().await.unwrap();
+}
