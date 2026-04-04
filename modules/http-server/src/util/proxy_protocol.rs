48e50e427efff40d6ba35a29c9039b7dd4f7f127
Dorian Niemiec <dorian@ferron.sh>
2026-04-04 09:50:30 +0200

    style: apply the code style

--- modules/http-server/src/util/proxy_protocol.rs
+++ modules/http-server/src/util/proxy_protocol.rs
@@ -32,137 +32,150 @@ const V2_LENGTH_INDEX: usize = 14;
 const READ_BUFFER_LEN: usize = 512;
 
 /// Reads the PROXY protocol header from the given `AsyncRead`.
-pub async fn read_proxy_header<I>(mut stream: I) -> Result<(I, Option<SocketAddr>, Option<SocketAddr>), std::io::Error>
+pub async fn read_proxy_header<I>(
+    mut stream: I,
+) -> Result<(I, Option<SocketAddr>, Option<SocketAddr>), std::io::Error>
 where
-  I: AsyncRead + Unpin,
+    I: AsyncRead + Unpin,
 {
-  // Mutable buffer for storing stream data
-  let mut buffer = [0; READ_BUFFER_LEN];
-  // Dynamic in case v2 header is too long
-  let mut dynamic_buffer = None;
+    // Mutable buffer for storing stream data
+    let mut buffer = [0; READ_BUFFER_LEN];
+    // Dynamic in case v2 header is too long
+    let mut dynamic_buffer = None;
 
-  // Read prefix to check for v1, v2, or kill
-  stream.read_exact(&mut buffer[..V1_PREFIX_LEN]).await?;
+    // Read prefix to check for v1, v2, or kill
+    stream.read_exact(&mut buffer[..V1_PREFIX_LEN]).await?;
 
-  if &buffer[..V1_PREFIX_LEN] == ppp::v1::PROTOCOL_PREFIX.as_bytes() {
-    read_v1_header(&mut stream, &mut buffer).await?;
-  } else {
-    stream.read_exact(&mut buffer[V1_PREFIX_LEN..V2_MINIMUM_LEN]).await?;
-    if &buffer[..V2_PREFIX_LEN] == ppp::v2::PROTOCOL_PREFIX {
-      dynamic_buffer = read_v2_header(&mut stream, &mut buffer).await?;
+    if &buffer[..V1_PREFIX_LEN] == ppp::v1::PROTOCOL_PREFIX.as_bytes() {
+        read_v1_header(&mut stream, &mut buffer).await?;
     } else {
-      return Err(std::io::Error::new(
-        std::io::ErrorKind::InvalidData,
-        "No valid Proxy Protocol header detected",
-      ));
-    }
-  }
-
-  // Choose which buffer to parse
-  let buffer = dynamic_buffer.as_deref().unwrap_or(&buffer[..]);
-
-  // Parse the header
-  let header = HeaderResult::parse(buffer);
-  match header {
-    HeaderResult::V1(Ok(header)) => {
-      let (client_address, server_address) = match header.addresses {
-        ppp::v1::Addresses::Tcp4(ip) => (
-          SocketAddr::new(IpAddr::V4(ip.source_address), ip.source_port),
-          SocketAddr::new(IpAddr::V4(ip.destination_address), ip.destination_port),
-        ),
-        ppp::v1::Addresses::Tcp6(ip) => (
-          SocketAddr::new(IpAddr::V6(ip.source_address), ip.source_port),
-          SocketAddr::new(IpAddr::V6(ip.destination_address), ip.destination_port),
-        ),
-        ppp::v1::Addresses::Unknown => {
-          // Return client address as `None` so that "unknown" is used in the http header
-          return Ok((stream, None, None));
+        stream
+            .read_exact(&mut buffer[V1_PREFIX_LEN..V2_MINIMUM_LEN])
+            .await?;
+        if &buffer[..V2_PREFIX_LEN] == ppp::v2::PROTOCOL_PREFIX {
+            dynamic_buffer = read_v2_header(&mut stream, &mut buffer).await?;
+        } else {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidData,
+                "No valid Proxy Protocol header detected",
+            ));
         }
-      };
-
-      Ok((stream, Some(client_address), Some(server_address)))
     }
-    HeaderResult::V2(Ok(header)) => {
-      let (client_address, server_address) = match header.addresses {
-        ppp::v2::Addresses::IPv4(ip) => (
-          SocketAddr::new(IpAddr::V4(ip.source_address), ip.source_port),
-          SocketAddr::new(IpAddr::V4(ip.destination_address), ip.destination_port),
-        ),
-        ppp::v2::Addresses::IPv6(ip) => (
-          SocketAddr::new(IpAddr::V6(ip.source_address), ip.source_port),
-          SocketAddr::new(IpAddr::V6(ip.destination_address), ip.destination_port),
-        ),
-        ppp::v2::Addresses::Unix(unix) => {
-          return Err(std::io::Error::new(
+
+    // Choose which buffer to parse
+    let buffer = dynamic_buffer.as_deref().unwrap_or(&buffer[..]);
+
+    // Parse the header
+    let header = HeaderResult::parse(buffer);
+    match header {
+        HeaderResult::V1(Ok(header)) => {
+            let (client_address, server_address) = match header.addresses {
+                ppp::v1::Addresses::Tcp4(ip) => (
+                    SocketAddr::new(IpAddr::V4(ip.source_address), ip.source_port),
+                    SocketAddr::new(IpAddr::V4(ip.destination_address), ip.destination_port),
+                ),
+                ppp::v1::Addresses::Tcp6(ip) => (
+                    SocketAddr::new(IpAddr::V6(ip.source_address), ip.source_port),
+                    SocketAddr::new(IpAddr::V6(ip.destination_address), ip.destination_port),
+                ),
+                ppp::v1::Addresses::Unknown => {
+                    // Return client address as `None` so that "unknown" is used in the http header
+                    return Ok((stream, None, None));
+                }
+            };
+
+            Ok((stream, Some(client_address), Some(server_address)))
+        }
+        HeaderResult::V2(Ok(header)) => {
+            let (client_address, server_address) = match header.addresses {
+                ppp::v2::Addresses::IPv4(ip) => (
+                    SocketAddr::new(IpAddr::V4(ip.source_address), ip.source_port),
+                    SocketAddr::new(IpAddr::V4(ip.destination_address), ip.destination_port),
+                ),
+                ppp::v2::Addresses::IPv6(ip) => (
+                    SocketAddr::new(IpAddr::V6(ip.source_address), ip.source_port),
+                    SocketAddr::new(IpAddr::V6(ip.destination_address), ip.destination_port),
+                ),
+                ppp::v2::Addresses::Unix(unix) => {
+                    return Err(std::io::Error::new(
+                        std::io::ErrorKind::InvalidData,
+                        format!("Unix socket addresses are not supported. Addresses: {unix:?}"),
+                    ));
+                }
+                ppp::v2::Addresses::Unspecified => {
+                    // Return client address as `None` so that "unknown" is used in the http header
+                    return Ok((stream, None, None));
+                }
+            };
+
+            Ok((stream, Some(client_address), Some(server_address)))
+        }
+        HeaderResult::V1(Err(_error)) => Err(std::io::Error::new(
             std::io::ErrorKind::InvalidData,
-            format!("Unix socket addresses are not supported. Addresses: {unix:?}"),
-          ));
-        }
-        ppp::v2::Addresses::Unspecified => {
-          // Return client address as `None` so that "unknown" is used in the http header
-          return Ok((stream, None, None));
-        }
-      };
-
-      Ok((stream, Some(client_address), Some(server_address)))
+            "No valid V1 Proxy Protocol header received",
+        )),
+        HeaderResult::V2(Err(_error)) => Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "No valid V2 Proxy Protocol header received",
+        )),
     }
-    HeaderResult::V1(Err(_error)) => Err(std::io::Error::new(
-      std::io::ErrorKind::InvalidData,
-      "No valid V1 Proxy Protocol header received",
-    )),
-    HeaderResult::V2(Err(_error)) => Err(std::io::Error::new(
-      std::io::ErrorKind::InvalidData,
-      "No valid V2 Proxy Protocol header received",
-    )),
-  }
 }
 
-async fn read_v2_header<I>(mut stream: I, buffer: &mut [u8; READ_BUFFER_LEN]) -> Result<Option<Vec<u8>>, std::io::Error>
+async fn read_v2_header<I>(
+    mut stream: I,
+    buffer: &mut [u8; READ_BUFFER_LEN],
+) -> Result<Option<Vec<u8>>, std::io::Error>
 where
-  I: AsyncRead + Unpin,
+    I: AsyncRead + Unpin,
 {
-  let length = u16::from_be_bytes([buffer[V2_LENGTH_INDEX], buffer[V2_LENGTH_INDEX + 1]]) as usize;
-  let full_length = V2_MINIMUM_LEN + length;
+    let length =
+        u16::from_be_bytes([buffer[V2_LENGTH_INDEX], buffer[V2_LENGTH_INDEX + 1]]) as usize;
+    let full_length = V2_MINIMUM_LEN + length;
 
-  // Switch to dynamic buffer if header is too long; v2 has no maximum length
-  if full_length > READ_BUFFER_LEN {
-    let mut dynamic_buffer = vec![0u8; full_length];
-    dynamic_buffer.extend_from_slice(&buffer[..V2_MINIMUM_LEN]);
+    // Switch to dynamic buffer if header is too long; v2 has no maximum length
+    if full_length > READ_BUFFER_LEN {
+        let mut dynamic_buffer = vec![0u8; full_length];
+        dynamic_buffer.extend_from_slice(&buffer[..V2_MINIMUM_LEN]);
 
-    // Read the remaining header length
-    stream
-      .read_exact(&mut dynamic_buffer[V2_MINIMUM_LEN..full_length])
-      .await?;
+        // Read the remaining header length
+        stream
+            .read_exact(&mut dynamic_buffer[V2_MINIMUM_LEN..full_length])
+            .await?;
 
-    Ok(Some(dynamic_buffer))
-  } else {
-    // Read the remaining header length
-    stream.read_exact(&mut buffer[V2_MINIMUM_LEN..full_length]).await?;
+        Ok(Some(dynamic_buffer))
+    } else {
+        // Read the remaining header length
+        stream
+            .read_exact(&mut buffer[V2_MINIMUM_LEN..full_length])
+            .await?;
 
-    Ok(None)
-  }
+        Ok(None)
+    }
 }
 
-async fn read_v1_header<I>(mut stream: I, buffer: &mut [u8; READ_BUFFER_LEN]) -> Result<(), std::io::Error>
+async fn read_v1_header<I>(
+    mut stream: I,
+    buffer: &mut [u8; READ_BUFFER_LEN],
+) -> Result<(), std::io::Error>
 where
-  I: AsyncRead + Unpin,
+    I: AsyncRead + Unpin,
 {
-  // Read one byte at a time until terminator found
-  let mut end_found = false;
-  for i in V1_PREFIX_LEN..V1_MAX_LENGTH {
-    buffer[i] = stream.read_u8().await?;
+    // Read one byte at a time until terminator found
+    let mut end_found = false;
+    for i in V1_PREFIX_LEN..V1_MAX_LENGTH {
+        buffer[i] = stream.read_u8().await?;
 
-    if [buffer[i - 1], buffer[i]] == V1_TERMINATOR {
-      end_found = true;
-      break;
+        if [buffer[i - 1], buffer[i]] == V1_TERMINATOR {
+            end_found = true;
+            break;
+        }
     }
-  }
-  if !end_found {
-    return Err(std::io::Error::new(
-      std::io::ErrorKind::InvalidData,
-      "No valid Proxy Protocol header detected",
-    ));
-  }
+    if !end_found {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "No valid Proxy Protocol header detected",
+        ));
+    }
 
-  Ok(())
+    Ok(())
 }
