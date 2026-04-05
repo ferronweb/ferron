94ca42d01baddc269dcef24bd167634375555c8c
Dorian Niemiec <dorian@ferron.sh>
2026-04-05 21:27:15 +0200

    chore: remove remnant custom 404 Not Found error page handler

--- modules/http-server/src/loader.rs
+++ modules/http-server/src/loader.rs
@@ -5,10 +5,10 @@ use std::sync::Arc;
 
 use ferron_core::loader::ModuleLoader;
 use ferron_core::registry::RegistryBuilder;
-use ferron_http::{HttpContext, HttpErrorContext};
+use ferron_http::HttpContext;
 
 use crate::server::BasicHttpModule;
-use crate::stages::{HelloStage, NotFoundStage};
+use crate::stages::HelloStage;
 use crate::validator::HttpConfigurationValidator;
 
 #[derive(Default)]
@@ -64,9 +64,7 @@ impl ModuleLoader for BasicHttpModuleLoader {
     }
 
     fn register_stages(&mut self, registry: RegistryBuilder) -> RegistryBuilder {
-        registry
-            .with_stage::<HttpContext, _>(|| Arc::new(HelloStage))
-            .with_stage::<HttpErrorContext, _>(|| Arc::new(NotFoundStage))
+        registry.with_stage::<HttpContext, _>(|| Arc::new(HelloStage))
     }
 
     fn register_modules(
--- modules/http-server/src/stages/mod.rs
+++ modules/http-server/src/stages/mod.rs
@@ -1,7 +1,5 @@
 //! HTTP pipeline stages
 
 mod hello;
-mod not_found;
 
 pub use hello::HelloStage;
-pub use not_found::NotFoundStage;
