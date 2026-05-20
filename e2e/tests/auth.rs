80dfed3cafdb64ed7927bef8a641230ad6f8fca0
Dorian Niemiec <dorian@ferron.sh>
2026-05-20 16:44:27 +0200

    test(e2e): add HTTP Basic Auth test for too many attempts

--- e2e/tests/auth.rs
+++ e2e/tests/auth.rs
@@ -171,3 +171,101 @@ async fn test_http_auth_failure() {
 
     container.stop().await.unwrap();
 }
+
+#[tokio::test]
+async fn test_http_auth_too_many_attempts() {
+    let _ = rustls::crypto::ring::default_provider().install_default();
+
+    #[cfg(unix)]
+    nix::sys::stat::umask(nix::sys::stat::Mode::from_bits(0o000).unwrap());
+
+    #[cfg(unix)]
+    let webroot_dir = tempfile::Builder::new()
+        .permissions(Permissions::from_mode(0o777))
+        .tempdir()
+        .unwrap();
+    #[cfg(unix)]
+    let mut config_file = tempfile::Builder::new()
+        .permissions(Permissions::from_mode(0o666))
+        .tempfile()
+        .unwrap();
+    #[cfg(not(unix))]
+    let webroot_dir = tempfile::tempdir().unwrap();
+    #[cfg(not(unix))]
+    let mut config_file = tempfile::NamedTempFile::new().unwrap();
+
+    let password_hash = "$argon2id$v=19$m=65536,t=3,p=1$c2VjcmV0c2FsdDEyMzQ1Njc4$R7dF5Q8QYJZQYJZQYJZQYJZQYJZQYJZQYJZQYJZQYJQ";
+
+    config_file
+        .as_file_mut()
+        .write_all(
+            format!(
+                r#"
+*:80 {{
+  basic_auth {{
+    realm "HTTP authentication test"
+    users {{
+      test "{password_hash}"
+    }}
+
+    brute_force_protection {{
+        enabled true
+        max_attempts 3
+        lockout_duration "15m"
+        window "5m"
+    }}
+  }}
+  root "/var/www/ferron"
+}}
+"#
+            )
+            .as_bytes(),
+        )
+        .unwrap();
+
+    self::common::write_file(webroot_dir.path().join("test.txt"), b"test content").unwrap();
+
+    let container = create_ferron_container(webroot_dir.path(), config_file.path())
+        .await
+        .unwrap();
+
+    let port = container
+        .get_host_port_ipv4(ContainerPort::Tcp(80))
+        .await
+        .unwrap();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get(format!("http://localhost:{}/test.txt", port))
+        .basic_auth("test", Some("wrong_password"))
+        .send()
+        .await
+        .unwrap();
+
+    assert_eq!(response.status(), reqwest::StatusCode::UNAUTHORIZED);
+
+    // 2 more attempts should fail before lockout
+    client
+        .get(format!("http://localhost:{}/test.txt", port))
+        .basic_auth("test", Some("wrong_password"))
+        .send()
+        .await
+        .unwrap();
+    client
+        .get(format!("http://localhost:{}/test.txt", port))
+        .basic_auth("test", Some("wrong_password"))
+        .send()
+        .await
+        .unwrap();
+
+    // 4th attempt should fail before lockout
+    let response = client
+        .get(format!("http://localhost:{}/test.txt", port))
+        .basic_auth("test", Some("wrong_password"))
+        .send()
+        .await
+        .unwrap();
+    assert_ne!(response.status(), reqwest::StatusCode::UNAUTHORIZED);
+
+    container.stop().await.unwrap();
+}
