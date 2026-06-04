eeb393e00d93916fd6235279391f5294848683e4
Dorian Niemiec <dorian@ferron.sh>
2026-06-04 21:24:33 +0200

    fix: fix compilation errors for Windows related to missing trace context

--- modules/metrics-process/src/windows.rs
+++ modules/metrics-process/src/windows.rs
@@ -151,6 +151,7 @@ fn emit_metrics(
         value: MetricValue::F64(cpu_user_time_increase),
         unit: Some("s"),
         description: Some("Total CPU seconds broken down by different states."),
+        trace_context: None,
     }));
 
     event_sink.emit(Event::Metric(MetricEvent {
@@ -163,6 +164,7 @@ fn emit_metrics(
         value: MetricValue::F64(cpu_system_time_increase),
         unit: Some("s"),
         description: Some("Total CPU seconds broken down by different states."),
+        trace_context: None,
     }));
 
     event_sink.emit(Event::Metric(MetricEvent {
@@ -175,6 +177,7 @@ fn emit_metrics(
             "Difference in process.cpu.time since the last measurement, \
              divided by the elapsed time and number of CPUs available to the process.",
         ),
+        trace_context: None,
     }));
 
     event_sink.emit(Event::Metric(MetricEvent {
@@ -190,6 +193,7 @@ fn emit_metrics(
             "Difference in process.cpu.time since the last measurement, \
              divided by the elapsed time and number of CPUs available to the process.",
         ),
+        trace_context: None,
     }));
 
     event_sink.emit(Event::Metric(MetricEvent {
@@ -199,6 +203,7 @@ fn emit_metrics(
         value: MetricValue::I64(rss_diff),
         unit: Some("By"),
         description: Some("The amount of physical memory in use."),
+        trace_context: None,
     }));
 
     event_sink.emit(Event::Metric(MetricEvent {
@@ -208,5 +213,6 @@ fn emit_metrics(
         value: MetricValue::I64(vms_diff),
         unit: Some("By"),
         description: Some("The amount of committed virtual memory."),
+        trace_context: None,
     }));
 }
