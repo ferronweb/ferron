dee7cbdb40ce8278af166b1db5d99bc1c6894ab9
Dorian Niemiec <dorian@ferron.sh>
2026-08-03 20:21:59 +0200

    Revert "refactor: replace `write!` with `write_str` for ArrayString"
    This reverts commit 3448aaaa33c8645fa131eee013d51699b73aa1ed.

--- modules/http-proxy/src/proxy/request.rs
+++ modules/http-proxy/src/proxy/request.rs
@@ -113,9 +113,9 @@ pub(super) fn construct_proxy_request(
 
     // Format IP addresses into stack-allocated buffers to avoid heap allocation
     let mut client_ip_buf = ArrayString::<45>::new();
-    let _ = client_ip_buf.write_str(&client_ip.to_string());
+    let _ = write!(client_ip_buf, "{}", client_ip);
     let mut local_ip_buf = ArrayString::<45>::new();
-    let _ = local_ip_buf.write_str(&local_ip.to_string());
+    let _ = write!(local_ip_buf, "{}", local_ip);
 
     let client_ip_from_header_enabled = ClientIpFromHeaderConfig::resolve_from_context(ctx)
         .is_some_and(|s| s.is_trusted_proxy(ctx.remote_address.ip()));
