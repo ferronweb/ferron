6183eec8dcebc8750adc59e2798d3d0ffff01640
Dorian Niemiec <dorian@ferron.sh>
2026-08-08 20:42:58 +0200

    fix(otlp): fix more validator conditions causing failing e2e tests

--- modules/observability-otlp/src/validator.rs
+++ modules/observability-otlp/src/validator.rs
@@ -23,7 +23,7 @@ impl ConfigurationValidator for OtlpObservabilityConfigurationValidator {
             validate_nested!(logs, used(sub), export_batch_size, optional args(1) => [ServerConfigurationValue::Number(_, _)]);
             if let Some(entries) = logs.directives.get("export_batch_size") {
                 for entry in entries {
-                    if entry.get_value().and_then(|v| v.as_number()).is_some_and(|n| n <= 1) {
+                    if entry.get_value().and_then(|v| v.as_number()).is_some_and(|n| n < 1) {
                         return Err(ConfigurationValidationError::from(format!(
                             "Export batch size for OTLP logs cannot be smaller than 1"
                         ))
